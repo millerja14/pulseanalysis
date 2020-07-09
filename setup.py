f0d5c348e7b3c7a321bea05082fcce3a140be924
millerja14 <jmmiller95616@gmail.com>
2020-07-09 15:06:37 -0700

    spelling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='pulseanalysis',
 	version='0.1',
-	description='Code for analysing pulse shapes from MKID data',
+	description='Code for analyzing pulse shapes from MKID data',
 	url='https://github.com/millerja14/pulseanalysis',
 	author='Jacob Miller',
 	license='GNU GPLv3',
