e400c20e19557d41fc216361df49ac85f0aa245e
millerja14 <jmmiller95616@gmail.com>
2020-07-09 15:04:20 -0700

    added project github url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='pulseanalysis',
 	version='0.1',
 	description='Code for analysing pulse shapes from MKID data',
-	url='',
+	url='https://github.com/millerja14/pulseanalysis',
 	author='Jacob Miller',
 	license='GNU GPLv3',
 	packages=find_packages(),
