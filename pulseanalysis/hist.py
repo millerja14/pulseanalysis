efc6c5c072dd3cfd1328432183d109a5d5803f73
millerja14 <jmmiller95616@gmail.com>
2020-07-10 13:46:56 -0700

    added plotting when calculating kde per peak

--- pulseanalysis/hist.py
+++ pulseanalysis/hist.py
@@ -109,9 +109,15 @@ def testBW(data, desc=""):
 	
 	plt.show()
 	
-def getFWHM(data, drawPlot=False):
+def getFWHM(data, samples=1000, drawPlot=False):
+	'''
+	Takes in distribution data, number of samples used to resolve the fit, and
+	drawPlot. Outputs array containing fwhm for each peak and the kde distribution
+	containing "samples" number of data points.
+	'''
+
 	# find peaks in data
-	x = np.linspace(np.amin(data), np.amax(data), 1000)
+	x = np.linspace(np.amin(data), np.amax(data), samples)
 	kernel = stats.gaussian_kde(data)
 	dist = kernel(x)
 	peak_indices, properties = find_peaks(dist, prominence=0, height=0)
@@ -136,8 +142,6 @@ def getFWHM(data, drawPlot=False):
 	
 	# compute variance
 	fwhm = slope*widths[0]
-	#print("Estimated FWHM of Peak #1: {:.4f} Units".format(fwhm[0]))
-	#print("Estimated FWHM of Peak #2: {:.4f} Units".format(fwhm[1]))
 	
 	if drawPlot:
 		fig = plt.figure()
@@ -148,16 +152,17 @@ def getFWHM(data, drawPlot=False):
 		ax.hlines(widths[1], widths[2]*slope+x[0], widths[3]*slope+x[0], color="C2")
 		plt.show()
 
-	return fwhm
+	return fwhm, dist
 
-def getFWHM_separatePeaks(data, npeaks=None, drawPlot=False):
+def getFWHM_separatePeaks(data, npeaks=None, samples=1000, drawPlot=True):
+	
 	# find minimums
-	x = np.linspace(np.amin(data), np.amax(data), 1000)
+	x = np.linspace(np.amin(data), np.amax(data), samples)
 	kernel = stats.gaussian_kde(data)
 	dist = kernel(x)
-	cutoff_indices, properties = find_peaks(-dist, prominence=0)
+	cutoff_indices, cutoff_properties = find_peaks(-dist, prominence=0)
 
-	peak_indices, _ = find_peaks(dist)
+	peak_indices, peak_properties = find_peaks(dist, height=0)
 	npeaks_total = peak_indices.size
 	
 	# ensure npeaks is valid
@@ -171,11 +176,18 @@ def getFWHM_separatePeaks(data, npeaks=None, drawPlot=False):
 		print("Can only find {0} peaks in the data. Assuming {0} peaks instead of {1}.".format(npeaks_total, npeaks))
 		npeaks = npeaks_total
 
+	# get npeaks greatest peak heights
+	peak_heights = peak_properties["peak_heights"]
+	idx = np.argsort(peak_heights)[-npeaks:]
+	peak_indices_filtered = np.sort(np.take(peak_indices, idx))
+	peak_heights_filtered = dist[peak_indices_filtered]
+	rel_peak_heights = peak_heights_filtered/np.sum(peak_heights_filtered)	
+
 	# get most prominent minimums
 	k = npeaks-1
-	prominences = properties["prominences"]
-	idx = np.argpartition(prominences, k)
-	cutoff_indices_filtered = cutoff_indices[idx[-k:]]
+	cutoff_prominences = cutoff_properties["prominences"]
+	idx = np.argsort(cutoff_prominences)[-k:]
+	cutoff_indices_filtered = np.sort(np.take(cutoff_indices, idx))
 	
 	# points at which we will split the data	
 	cutoffs = x[cutoff_indices_filtered]
@@ -184,20 +196,25 @@ def getFWHM_separatePeaks(data, npeaks=None, drawPlot=False):
 
 	# split the data into peaks
 	data_split = []
-	fwhm = np.zeros(cutoffs.size-1)
+	fwhm_list = []
+	dist_list = []
+
 	for i in range(cutoffs.size - 1):
 		data_split.append(data[(data>cutoffs[i]) & (data<=cutoffs[i+1])])
-		fwhm[i] = getFWHM(data_split[i], drawPlot)
+		fwhm, dist = getFWHM(data_split[i], samples, drawPlot=False)
+		fwhm_list.append(fwhm)
+		dist_list.append(dist)
 
-	return fwhm
-	
-	#if drawPlot:
-	#	fig = plt.figure()
-	#	ax = fig.add_subplot(111)
-	#	ax.hist(data, bins='auto', density=True)
-	#	ax.plot(x, dist)
-	#	ax.plot(x[cutoff_indices_filtered], dist[cutoff_indices_filtered], "x")
-	#	plt.show()
+	if drawPlot:
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+		ax.hist(data, bins='auto', density=True)
+		for sdata, dist, height in zip(data_split, dist_list, rel_peak_heights):
+			x = np.linspace(np.amin(sdata), np.amax(sdata), samples)
+			ax.plot(x, dist*height)
+		plt.show()
+
+	return np.array(fwhm_list)
 
 def compareDist(data1, data2, nbin=300, drawPlot='True'):
 	fwhm1 = getFWHM(data1)
